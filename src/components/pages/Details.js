652e3a14c5f41002e16587fddb9d5193c0410ed9
katfogy <katfogy@gmail.com>
2023-06-27 16:43:47 +0100

    single page done

--- src/components/pages/Details.js
+++ src/components/pages/Details.js
@@ -1,7 +1,57 @@
 import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { BiArrowBack } from 'react-icons/bi';
+import { FaBitcoin } from 'react-icons/fa';
 
-const Details = () => (
-  <div className="container">Details</div>
-);
+const Details = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const backHome = () => {
+    navigate('/');
+  };
+
+  const { coins } = location.state || { coins: {} };
+  return (
+    <div className="container">
+      <div className="details">
+        <BiArrowBack onClick={() => backHome()} />
+        <h2 className="detail-title">
+          {coins.name}
+          {' '}
+          {coins.symbol}
+        </h2>
+        <div className="details-body">
+          <p className="market-cap">
+            Market Cap: $
+            {Number(coins.marketCapUsd).toLocaleString(undefined, {
+              maximumFractionDigits: 2,
+            })}
+          </p>
+          <p className="price">
+            Price(Btc):
+            {' '}
+            <FaBitcoin />
+            {' '}
+            {coins.priceUsd}
+          </p>
+          <p className="t24hr">
+            24h %:
+            {' '}
+            {Number(coins.maxSupply).toLocaleString(undefined, {
+              maximumFractionDigits: 2,
+            })}
+          </p>
+          <p className="volume24">
+            Volume(24h): $
+            {Number(coins.volumeUsd24Hr).toLocaleString(undefined, {
+              maximumFractionDigits: 2,
+            })}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Details;
