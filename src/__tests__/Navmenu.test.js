27ae69444bb2c16cafecbe3c03376edfae55d6a8
katfogy <katfogy@gmail.com>
2023-06-29 10:44:19 +0100

    linters error fixed

--- src/__tests__/Navmenu.test.js
+++ src/__tests__/Navmenu.test.js
@@ -1,6 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
-import Navmenu from '../components/Navmenu';
+import Navmenu from '../components/NavMenu.js';
 
 it('renders correctly', () => {
   const tree = renderer
